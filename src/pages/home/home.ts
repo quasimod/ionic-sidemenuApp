91260d4a59fae62482eb413980cf53f4658fd770
quasimod <quasimod80@gmail.com>
2018-05-30 21:00:04 +0900

    second

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
+import { Profile } from '../../interfaces/Profile';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  public profile = {} as Profile;
+  constructor(public navCtrl: NavController, public modalCtrl:ModalController) {
 
   }
-
+  modal(){
+    let modal = this.modalCtrl.create('ModalPage');
+    modal.onDidDismiss(data => {
+      this.profile = data;
+      console.log(data);
+    });
+    modal.present();
+  }
 }
