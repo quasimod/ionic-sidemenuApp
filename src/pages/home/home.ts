3fee80af4f5b066b94d9758fdc651d93dd5cedd7
quasimod <quasimod80@gmail.com>
2018-05-31 18:52:48 +0900

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import { NavController, ModalController, AlertController } from 'ionic-angular';
 import { Profile } from '../../interfaces/Profile';
 
 @Component({
@@ -8,15 +8,44 @@ import { Profile } from '../../interfaces/Profile';
 })
 export class HomePage {
   public profile = {} as Profile;
-  constructor(public navCtrl: NavController, public modalCtrl:ModalController) {
+  constructor(public navCtrl: NavController, public modalCtrl:ModalController, public alertCtrl:AlertController) {
 
   }
   modal(){
     let modal = this.modalCtrl.create('ModalPage');
     modal.onDidDismiss(data => {
-      this.profile = data;
-      console.log(data);
+      if(data != null) {
+        this.profile = data;
+        console.log(data);
+      }
     });
     modal.present();
+  }
+
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Login',
+      message: "Enter a name for this new album you're so keen on adding",
+      inputs: [
+        { name: 'name', placeholder: 'Name 입력' },
+        { name: 'email', placeholder: 'Email 입력' },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log(data.name);
+            console.log(data.email);
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 }
